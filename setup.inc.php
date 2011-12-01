abff379c97f41b03a5b9c29a1c1a1269cb5c26cf
user <user@69e63612-969a-4371-9866-b14aae95a0cb>
2011-12-01 02:03:23 +0000

    Modifications module org_lucterios_task (1.2.1.532)
    git-svn-id: http://projets.lucterios.org/svn/extensions/org_lucterios_task@1494 69e63612-969a-4371-9866-b14aae95a0cb

--- setup.inc.php
+++ setup.inc.php
@@ -18,7 +18,7 @@
 // 
 // 		Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
 // setup file write by SDK tool
-// --- Last modification: Date 29 November 2011 19:53:13 By  ---
+// --- Last modification: Date 01 December 2011 0:27:49 By  ---
 
 $extention_name="org_lucterios_task";
 $extention_description="Modules de gestion de projets organisés en taches de travail {[newline]}Il permet également de suivre leur évolution.";
@@ -30,7 +30,7 @@ $extension_libre=true;
 $version_max=1;
 $version_min=2;
 $version_release=1;
-$version_build=531;
+$version_build=532;
 
 $depencies=array();
 $depencies[0] = new Param_Depencies("CORE", 1, 3, 1, 3, false);
