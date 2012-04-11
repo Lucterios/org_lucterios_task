a421ab8298d866b4c403d40dcb97722029698098
lag <lag@69e63612-969a-4371-9866-b14aae95a0cb>
2012-04-11 11:33:37 +0000

    Nouvelle version
    git-svn-id: http://projets.lucterios.org/svn/extensions/org_lucterios_task@1684 69e63612-969a-4371-9866-b14aae95a0cb

--- setup.inc.php
+++ setup.inc.php
@@ -17,7 +17,7 @@
 // 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // 
 // 		Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY// setup file write by SDK tool
-// --- Last modification: Date 28 March 2012 12:21:19 By  ---
+// --- Last modification: Date 11 April 2012 4:20:09 By  ---
 
 $extention_name="org_lucterios_task";
 $extention_description="Modules de gestion de projets organisés en taches de travail {[newline]}Il permet également de suivre leur évolution.";
@@ -28,7 +28,7 @@ $extension_libre=true;
 
 $version_max=1;
 $version_min=2;
-$version_release=3;
+$version_release=4;
 $version_build=580;
 
 $depencies=array();
